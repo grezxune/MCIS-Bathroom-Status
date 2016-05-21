536dee1aa79d0f24a08d4ec485b3ee135e68d828
tommy <tomtrezb2003@gmail.com>
2016-05-21 12:13:29 -0500

    Added the use of the template, as well as generated a requirements.txt file for virtual environments to get dependencies needed. This allows us to not need to put the entire virtual environment in the repo, and allow a single command install of all dependencies needed.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8000')
